e227537469485c2fb59ecf3ee073f254e13271fd
Said <caidcode@gmail.com>
2025-02-27 03:30:48 +0500

    categoryDetail

---
+++ lib/core/l10n/app_localizations_en.dart
@@ -0,0 +1,40 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class MyLocalizationsEn extends MyLocalizations {
+  MyLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get signUp => 'Sign Up';
+
+  @override
+  String get fullName => 'Full Name';
+
+  @override
+  String get lastName => 'Last Name';
+
+  @override
+  String get userName => 'User Name';
+
+  @override
+  String get email => 'Email';
+
+  @override
+  String get mobileNumber => 'Mobile Number';
+
+  @override
+  String get dateOfBirth => 'Date Of Birth';
+
+  @override
+  String get password => 'Password';
+
+  @override
+  String get confirmPassword => 'Confirm Password';
+
+  @override
+  String get signUpButton => 'Sign Up';
+}
---
+++ lib/core/l10n/app_localizations_uz.dart
@@ -0,0 +1,40 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Uzbek (`uz`).
+class MyLocalizationsUz extends MyLocalizations {
+  MyLocalizationsUz([String locale = 'uz']) : super(locale);
+
+  @override
+  String get signUp => 'Ro\'yxatdan o\'tish';
+
+  @override
+  String get fullName => 'To\'liq Ism';
+
+  @override
+  String get lastName => 'Familiya';
+
+  @override
+  String get userName => 'Foydalanuvchi Nomi';
+
+  @override
+  String get email => 'Elektron Pochta';
+
+  @override
+  String get mobileNumber => 'Mobil Raqam';
+
+  @override
+  String get dateOfBirth => 'Tug\'ulgan Kuni';
+
+  @override
+  String get password => 'Parol';
+
+  @override
+  String get confirmPassword => 'Parolni Tasdiqlang';
+
+  @override
+  String get signUpButton => 'Ro\'yxatdan o\'tish';
+}
